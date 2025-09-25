2db63c7bdcdbc954a2a16a6048c64a7db205e90d
Lohran Mendes <lohranfm@gmail.com>
2025-09-25 10:26:31 -0300

    add address table constants; define fields for address management

---
+++ src/main/java/edu/infnet/repository/address/AddressTable.java
@@ -0,0 +1,10 @@
+package edu.infnet.repository.address;
+
+public class AddressTable {
+    public static final int ADDRESS_ID = 0;
+    public static final int DISTRICT = 1;
+    public static final int CITY = 2;
+    public static final int POSTAL_CODE = 3;
+    public static final int STREET = 4;
+    public static final int NUMBER = 5;
+}
